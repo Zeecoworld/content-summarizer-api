feed4521bc96ab77edb9192abc98c89542a886dc
Isaac <engrisaac1234@gmail.com>
2021-11-15 19:17:14 +0100

    added little fix

--- fileproject/settings.py
+++ fileproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_=r)yg_sfl(a+qho!(rxft$kj07u*o##a-5--+95uod9pok@$s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['summarizer.herokuapp.com']
 
 
 # Application definition
